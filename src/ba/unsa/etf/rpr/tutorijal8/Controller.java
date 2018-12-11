2440585135fc1bc81490ee12457633fb4a41d6e7
student <https://github.com/imaticevic1>
2018-12-11 18:29:48 +0100

    implementiorao run metodu

--- src/ba/unsa/etf/rpr/tutorijal8/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal8/Controller.java
@@ -1,46 +1,73 @@
 package ba.unsa.etf.rpr.tutorijal8;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Controller implements Runnable{
-    public Controller(){}
+
+    public Button prekiniDugme;
     public TextField poljeTeksta;
-    public ListView lista;
+    public ListView lista=new ListView();
     public Button traziDugme;
+    public boolean dugmeKliknuto = false;
+    public boolean isPrekini = false;
     private Set<String> stringSet = new HashSet<String>();
     private ObservableList<String> itemList = FXCollections.observableArrayList();
+
     private void pretrazi(File file){
-        if(file.getName().contains(poljeTeksta.getText()))
-            stringSet.add(file.getAbsolutePath());
+        if(file.getName().contains(poljeTeksta.getText())) {
+            String absolutePath = file.getAbsolutePath();
+            //stringSet.add(absolutePath);
+            Platform.runLater(() -> lista.getItems().add(absolutePath));
+            //itemList.add(absolutePath);
+        }
         if (!file.exists())
-            System.out.println("Datoteka ne postoji.");
+            System.out.println("");
         else if (file.isDirectory())
             for(File ime : file.listFiles())
                 pretrazi(ime);
     }
-    public void trazi(ActionEvent actionEvent) {
+    public void traziButton(ActionEvent actionEvent) {
+        dugmeKliknuto = true;
+        File file = new File(System.getProperty("user.home"));
         itemList.clear();
-        File file = new File("C:\\Users\\Goran\\IdeaProjects");
         Controller daljinski = new Controller();
-        Thread nit = new Thread(daljinski);
         daljinski.poljeTeksta = poljeTeksta;
         daljinski.pretrazi(file);
+        Thread nit = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                prekiniDugme.setDisable(false);
+                traziDugme.setDisable(true);
+                pretrazi(file);
+                traziDugme.setDisable(false);
+                if(isPrekini){
+                    prekiniDugme.setDisable(true);
+                    return;
+                }
+                prekiniDugme.setDisable(true);
+            }
+        });
         nit.start();
-        itemList.addAll(daljinski.stringSet);
-        lista.setItems(itemList);
+        //itemList.addAll(daljinski.stringSet);
+        //lista.setItems(itemList);
     }
 
-    @Override
-    public void run() {
 
+    public void run() {
+        if(dugmeKliknuto)
+            traziDugme.setDisable(true);
+    }
+
+    public void prekiniDugme(ActionEvent actionEvent) {
+        isPrekini = true;
     }
 }
